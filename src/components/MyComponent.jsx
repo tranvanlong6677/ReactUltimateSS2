c07afa0b2a6ed9c2c0c1c371e50225ac2b44c8b2
“Tran <tranvanlong6677@gmail.com>
2022-10-19 22:07:34 +0700

    bai 19

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -7,10 +7,16 @@ class MyComponent extends React.Component {
     address: "Hanoi",
     age: 21,
   };
+  handleClickBtn(event) {
+    console.log(event);
+    console.log(this.state.name);
+  }
   render() {
     return (
       <div>
         <div>My name is {this.state.name}</div>
+        <button onMouseOver={this.handleClickBtn}>Hover me</button>
+        <button onClick={this.handleClickBtn}>Click me</button>
       </div>
     );
   }
