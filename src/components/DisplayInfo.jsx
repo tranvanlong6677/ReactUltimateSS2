015bb7d08e902320b8d9646d12b03091fe72dd95
“Tran <tranvanlong6677@gmail.com>
2022-10-25 22:09:04 +0700

    bai 35

--- src/components/DisplayInfo.jsx
+++ src/components/DisplayInfo.jsx
@@ -1,46 +1,21 @@
 import React from "react";
 import "./DisplayInfo.scss";
 import { useState } from "react";
-// class DisplayInfo extends React.Component {
-//   handleShowHide = () => {
-//     this.setState({
-//       show: !this.state.show,
-//     });
-//     // alert("me");
-//   };
-
-//   render() {
-//     const { listUsers } = this.props;
-//     console.log("render");
-//     return (
-//       <div className="display-info-container">
-//         {true && (
-//           <div>
-//             {listUsers.map((item) => {
-//               return (
-//                 <div key={item.id}>
-//                   <div className={item.age > 26 ? "green" : "red"}>
-//                     My name is {item.name} and my age is {item.age}
-//                   </div>
-//                   <button onClick={() => this.props.handleDeleteUser(item.id)}>
-//                     Delete
-//                   </button>
-//                   <hr />
-//                 </div>
-//               );
-//             })}
-//           </div>
-//         )}
-//       </div>
-//     );
-//   }
-// }
 
 const DisplayInfo = (props) => {
   const { listUsers } = props;
+  const [isShowHideListUser, setIsShowHideListUser] = useState(true);
+  const handleShowHideListUser = () => {
+    setIsShowHideListUser(!isShowHideListUser);
+  };
   return (
     <div className="display-info-container">
-      {true && (
+      <div>
+        <button onClick={() => handleShowHideListUser()}>
+          {isShowHideListUser ? "Hide list user" : "Show list user"}
+        </button>
+      </div>
+      {isShowHideListUser && (
         <div>
           {listUsers.map((item) => {
             return (
