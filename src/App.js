b9aa41892bb03a67169fee46786f3622d671d62a
“Tran <tranvanlong6677@gmail.com>
2022-10-31 01:55:41 +0700

    bai 46

--- src/App.js
+++ src/App.js
@@ -1,39 +1,7 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
-import React from "react";
 
-// const App = () => {
-//   const count = useSelector((state) => state.counter.count);
-//   const dispatch = useDispatch();
-
-// return (
-// <div className="App">
-//   <header className="App-header">
-//     <img src={logo} className="App-logo" alt="logo" />
-//     <p>Hello world</p>
-//     <div>Count = {count}</div>
-//     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//   </header>
-// </div>
-//     <div>
-//       Hello world
-//       <MyComponent />
-//     </div>
-//   );
-// };
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <MyComponent />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <div className="app-container">Hello world</div>;
+};
 
 export default App;
