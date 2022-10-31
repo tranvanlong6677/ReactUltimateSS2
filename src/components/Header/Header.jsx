4689152b430cac4890de53e262da2fbd9e79ac79
“Tran <tranvanlong6677@gmail.com>
2022-11-01 03:50:14 +0700

    bai 53

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,33 +6,36 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <NavLink to="/" className="nav-link navbar-brand">
-          My App
-        </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavLink to="/" className="nav-link">
-              Home
-            </NavLink>
-            <NavLink to="/users" className="nav-link">
-              Users
-            </NavLink>
-            <NavLink to="/admins" className="nav-link">
-              Admin
-            </NavLink>
-          </Nav>
-          <Nav>
-            <NavDropdown title="Setting" id="basic-nav-dropdown">
+    <Navbar bg="light" expand="lg" className="px-5">
+      {/* <Container> */}
+      <NavLink to="/" className="nav-link navbar-brand">
+        My App
+      </NavLink>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <NavLink to="/" className="nav-link">
+            Home
+          </NavLink>
+          <NavLink to="/users" className="nav-link">
+            Users
+          </NavLink>
+          <NavLink to="/admins" className="nav-link">
+            Admin
+          </NavLink>
+        </Nav>
+        <Nav>
+          <button className="btn btn-login btn-light">Login</button>
+          <button className="btn btn-signup btn-dark mx-3">Sign up</button>
+
+          {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
               <NavDropdown.Item>Login</NavDropdown.Item>
               <NavDropdown.Item>Logout</NavDropdown.Item>
               <NavDropdown.Item>Profile</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+            </NavDropdown> */}
+        </Nav>
+      </Navbar.Collapse>
+      {/* </Container> */}
     </Navbar>
   );
 };
--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -6,6 +6,16 @@ const HomePage = () => {
       <video autoPlay muted loop>
         <source src={videoHomePage} type="video/mp4" />
       </video>
+      <div className="homepage-content">
+        <div className="title-1">There's a better way to ask</div>
+        <div clasName="title-2">
+          You don't want to make a boring form. And your audience won't answer
+          one. Create a typeform instead—and make everyone happy.
+        </div>
+        <div className="title-3">
+          <button className="btn btn-dark">Get started - It's free</button>
+        </div>
+      </div>
     </div>
   );
 };
