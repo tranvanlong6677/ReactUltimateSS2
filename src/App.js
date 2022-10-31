d79825f3eeb04ed9671891e89f6e5824f1ed311b
“Tran <tranvanlong6677@gmail.com>
2022-11-01 02:46:18 +0700

    bai 49

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 const App = () => {
   return (
     <div className="app-container">
-      <Header />
-      <div>
-        test link
-        <div>
-          <button>
-            <Link to="/users">Go to user page</Link>
-          </button>
-          <button>
-            <Link to="/admins">Go to admin page</Link>
-          </button>
+      <div className="header-container">
+        <Header />
+      </div>
+      <div className="main-container">
+        <div className="sidenav-container"></div>
+        <div className="app-content">
+          <Outlet />
+          {/* Khi nào vào 1 Route con nào thì component của Route con ấy sẽ thay thế cho Outlet
+            Outlet sẽ được viết ở component cha
+            Ở đây component App là cha nên sẽ viết Outlet ở đây
+            Khi vào /users hay /admins thì component con tương ứng sẽ hiển thị thay thế cho Outlet
+            Nhờ Outlet và nested route mà phần header sẽ luôn hiển thị khi các component con được render
+          */}
         </div>
       </div>
     </div>
