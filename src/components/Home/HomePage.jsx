d2ea25b432cae775c6db9e068eff79da9c3379fd
“Tran <tranvanlong6677@gmail.com>
2022-11-01 03:20:20 +0700

    bai 52

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,5 +1,13 @@
+import videoHomePage from "../../assets/Homepage.mp4";
+
 const HomePage = () => {
-  return <div className="homepage-container">Home page</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop>
+        <source src={videoHomePage} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 
 export default HomePage;
