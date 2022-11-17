247645955a624a8129076000d0d2a50ed56b5e79
“Tran <tranvanlong6677@gmail.com>
2022-11-17 13:58:08 +0700

    bai80

--- src/index.js
+++ src/index.js
@@ -2,22 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
 import "nprogress/nprogress.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     {/* <React.StrictMode> */}
     {/* <ProSidebarProvider> */}
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </PersistGate>
     {/* </ProSidebarProvider> */}
-
     {/* </React.StrictMode> */}
   </Provider>
 );
