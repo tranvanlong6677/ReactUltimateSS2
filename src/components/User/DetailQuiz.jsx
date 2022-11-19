85d30d392255da5e765328843544332b3c67f442
“Tran <tranvanlong6677@gmail.com>
2022-11-20 05:30:26 +0700

    bai 83

--- src/components/User/DetailQuiz.jsx
+++ src/components/User/DetailQuiz.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiServices";
+import _ from "lodash";
 
 const DetailQuiz = () => {
   const [dataQuiz, setDataQuiz] = useState([]);
@@ -10,11 +11,36 @@ const DetailQuiz = () => {
     fetchQuestions();
   }, [quizId]);
   const fetchQuestions = async () => {
-    let data = await getDataQuiz(quizId);
-    console.log(data.DT);
-    setDataQuiz(data.DT);
+    let res = await getDataQuiz(quizId);
+    setDataQuiz(res.DT);
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+      let data = _.chain(raw)
+        // Group the elements of Array based on `color` property
+        .groupBy("id")
+        // `key` is group's name (color), `value` is the array of objects
+        .map((value, key) => {
+          let answers = [];
+          let questionDescription,
+            image = null;
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDescription = item.description;
+              image = item.image;
+            }
+            answers.push(item.answers);
+          });
+
+          return {
+            questionId: key,
+            answers,
+            questionDescription,
+            image,
+          };
+        })
+        .value();
+    }
   };
-  console.log(params);
   return (
     dataQuiz &&
     dataQuiz.length !== 0 &&
